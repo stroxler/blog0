2e4da893a7b550517c29c4e20f46ede16c9c8dba
Steven Troxler <steven.troxler@gmail.com>
2015-12-21 18:26:34 -0800

    Ad linear regression post, also a post on a brew / R issue

--- knitr2wp_send.R
+++ knitr2wp_send.R
@@ -1,15 +1,18 @@
-# This is my script for pushing up my initial blog post, which was on installing
-# R and RStudio on ubuntu, to my wordpress blog.
-#
-# I pretty much obtained this script from the blog post at
-#   http://yihui.name/knitr/demo/wordpress/
-# although I had to modify the wordpress_login a bit since my username isn't
-# a valid R keyword argument
-library(RWordPress)
-library(knitr)
-wordpress_login = c('this_is_where_my_admin_wordpress_login_goes')
-names(wordpress_login) = c('steven.troxler@gmail.com')
-options(WordpressLogin = wordpress_login,
-        WordpressURL = 'http://www.steventroxler.com/blog/xmlrpc.php')
-knit2wp('~/blog0/islr/ch1.Rmd',
-        title = 'An Introduction to Statistical Learning in R, Chapter 1: Visualizing Datasets')
+
+# You can't get RWordPress from the default repo. Here's the code to get it
+#   from omegahat.
+# install.packages("RWordPress", repos = "http://www.omegahat.org/R")
+
+# This approach to posting I borrowed from
+#   http://yihui.name/knitr/demo/wordpress/.
+# Unfortunately, I bumped R versions to 3.2 and RWordPress isn't supported yet.
+post_to_blog <- function (login, path_to_file, title) {
+  library(RWordPress)
+  library(knitr)
+  wordpress_login = c(login)
+  names(wordpress_login) = c('steven.troxler@gmail.com')
+  options(WordpressLogin = wordpress_login,
+          WordpressURL = 'http://www.steventroxler.com/blog/xmlrpc.php')
+  knit2wp(path_to_file,
+          title = title)
+}
