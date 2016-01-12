145a049393d81a6363d4a54b8cbc11103f55b8c3
Steven Troxler <steven.troxler@gmail.com>
2016-01-11 16:27:29 -0800

    Improve the sent to wp a bit

--- R/knitr2wp_send.R
+++ R/knitr2wp_send.R
@@ -1,13 +1,10 @@
-
-# You can't get RWordPress from the default repo. Here's the code to get it
-#   from omegahat.
-# install.packages("RWordPress", repos = "http://www.omegahat.org/R")
-
 # This approach to posting I borrowed from
 #   http://yihui.name/knitr/demo/wordpress/.
-# Unfortunately, I bumped R versions to 3.2 and RWordPress isn't supported yet.
 post_to_blog <- function (login, path_to_file, title) {
-  library(RWordPress)
+  if(!require(RWordPress)) {
+    install.packages("RWordPress", repos = "http://www.omegahat.org/R", type='source')
+    require(RWordPress)
+  }
   library(knitr)
   wordpress_login = c(login)
   names(wordpress_login) = c('steven.troxler@gmail.com')
